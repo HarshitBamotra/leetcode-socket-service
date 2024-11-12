84b8b5e428cfbe6d0d53bae3ac536e824e5892f3
Harshit Bamotra <harshitbamotra.01@gmail.com>
2024-11-12 12:13:19 +0530

    Socket Service

---
+++ src/server.js
@@ -0,0 +1,75 @@
+// const express = require("express");
+// const { createServer } = require("http");
+// const { Server } = require("socket.io");
+
+// const app = express();
+// const httpServer = createServer(app);
+// const io = new Server(httpServer, { /* options */ });
+
+// io.on("connection", (socket) => {
+//   // ...
+// });
+
+// httpServer.listen(3003, ()=>{
+//   console.log("Server started on port 3003");
+// });
+
+
+const express = require("express"); // Import express
+const { createServer } = require("http"); // Import http
+const { Server } = require("socket.io"); // Import socket.io
+const Redis = require('ioredis');
+const bodyParser = require('body-parser');
+
+const app = express(); // Create express app
+app.use(bodyParser.json());
+const httpServer = createServer(app); // Create http server using express app
+
+const redisCache = new Redis(); // Create Redis client
+
+const io = new Server(httpServer, { 
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+ }); // Create socket.io server
+
+io.on("connection", (socket) => {
+    console.log("A user connected " + socket.id);
+    socket.on("setUserId", (userId) => {
+        console.log("Setting user id to connection id", userId, socket.id);
+        redisCache.set(userId, socket.id);
+    });
+
+    socket.on('getConnectionId', async (userId) => {
+        const connId = await redisCache.get(userId);
+        console.log("Getting connection id for user id", userId, connId);
+        socket.emit('connectionId', connId);
+        const everything = await redisCache.keys('*');
+        
+        console.log(everything)
+    })
+
+});
+
+app.post('/sendPayload', async (req, res) => {
+    console.log(req.body);
+    const { userId, payload } = req.body;
+   if(!userId || !payload) {
+       return res.status(400).send("Invalid request");
+   }
+   const socketId = await redisCache.get(userId);
+
+   if(socketId) {
+         io.to(socketId).emit('submissionPayloadResponse', payload);
+         return res.send("Payload sent successfully");
+    } else {
+        return res.status(404).send("User not connected");
+    
+   }
+
+})
+
+httpServer.listen(3004, () => {
+    console.log("Server is running on port 3004");
+});
